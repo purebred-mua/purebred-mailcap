831d4e7d0a92533aa98aebc288da6fa2010f77b1
Róman Joost <roman@bromeco.de>
2021-10-16 11:24:56 +1000

    Packaging changes

--- .nix/purebred-mailcap.nix
+++ .nix/purebred-mailcap.nix
@@ -1,12 +1,17 @@
-{ mkDerivation, attoparsec, base, lib }:
+{ mkDerivation, attoparsec, base, lib, tasty, tasty-hunit, text
+, time
+}:
 mkDerivation {
   pname = "purebred-mailcap";
   version = "0.1.0.0";
   src = ./..;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ attoparsec base ];
-  executableHaskellDepends = [ base ];
+  libraryHaskellDepends = [ attoparsec base text ];
+  executableHaskellDepends = [ base text ];
+  testHaskellDepends = [
+    attoparsec base tasty tasty-hunit text time
+  ];
   license = "unknown";
   hydraPlatforms = lib.platforms.none;
 }
