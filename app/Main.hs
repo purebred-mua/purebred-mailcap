e56fe6ae627448ba4ababd69a07a078267bd5a4f
Róman Joost <roman@bromeco.de>
2021-11-08 18:12:24 +1000

    Fix up main app

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,11 @@
 module Main where
 
 import System.Environment (getArgs)
-import qualified Data.Text.IO as T (getContents)
+import qualified Data.ByteString as B (getContents)
 import Data.Mailcap (parseMailcapfile)
 
 main :: IO ()
 main = do
-  print "accepting input"
-  contents <- T.getContents
-  print "parsing"
+  contents <- B.getContents
   let parsed = parseMailcapfile contents
   print $ show parsed
