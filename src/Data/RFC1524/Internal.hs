1fd76a02c85c45e7f0cd48f21e952ecd85350e85
Róman Joost <roman@bromeco.de>
2022-01-08 14:36:41 +1000

    Fix up formatting and trailing whitespace

--- src/Data/RFC1524/Internal.hs
+++ src/Data/RFC1524/Internal.hs
@@ -1,25 +1,26 @@
-module Data.RFC1524.Internal (
-  ContentType(..)
-  , parseContentType
-  , ci
-  , token) where
+module Data.RFC1524.Internal
+  ( ContentType (..),
+    parseContentType,
+    ci,
+    token,
+  )
+where
 
-import Data.String (IsString(fromString))
-import Data.Attoparsec.ByteString.Char8 (char8)
 import Data.Attoparsec.ByteString
-import qualified Data.ByteString.Char8 as C8
+import Data.Attoparsec.ByteString.Char8 (char8)
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C8
 import qualified Data.CaseInsensitive as CI
+import Data.String (IsString (fromString))
 
 -- borrowed from purebred-email
 data ContentType = ContentType (CI.CI B.ByteString) (CI.CI B.ByteString)
   deriving (Show, Eq)
 
-
 instance IsString ContentType where
   fromString = either err id . parseOnly parseContentType . C8.pack
-    where       
-    err msg = error $ "failed to parse Content-Type: " <> msg
+    where
+      err msg = error $ "failed to parse Content-Type: " <> msg
 
 parseContentType :: Parser ContentType
 parseContentType = do
@@ -32,9 +33,9 @@ parseContentType = do
 ci :: CI.FoldCase s => Parser s -> Parser (CI.CI s)
 ci = fmap CI.mk
 
--- | header token parser  
-token :: Parser B.ByteString     
-token =             
+-- | header token parser
+token :: Parser B.ByteString
+token =
   takeWhile1 (\c -> c >= 33 && c <= 126 && notInClass "()<>@,;:\\\"/[]?=" c)
 
 -- end --
